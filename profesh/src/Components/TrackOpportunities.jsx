df4ee34b0d19270abd84622a420522bd8dddac81
Tyler Huyser <tylerhuyser@tylers-mbp.myfiosgateway.com>
2020-09-07 13:05:04 -0400

    push up code

--- profesh/src/Components/TrackOpportunities.jsx
+++ profesh/src/Components/TrackOpportunities.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // import axios from 'axios';
-import OpportunitiesSearch from "./Tracker/OpportunitiesSearch.jsx";
-import AddOpportunity from "./Tracker/AddOpportunity.jsx";
+import OpportunitiesSearch from "./Tracker/OpportunitiesSearch";
+import AddOpportunity from "./Tracker/AddOpportunity";
 
 function TrackOpportunities() {
 
   return (
-    <div className="trackOpportunities">
+    <div className="tracker">
       <OpportunitiesSearch />
       <AddOpportunity />
     </div>
