7b98c71229378b844eeff41703493b675e18fe4c
Tyler Huyser <tylerhuyser@tylers-mbp.myfiosgateway.com>
2020-09-07 17:41:25 -0400

    appended opportunities to page

--- profesh/src/Components/Tracker/OpportunitiesSearch.jsx
+++ profesh/src/Components/Tracker/OpportunitiesSearch.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 import OpportunityResults from "./Search/OpportunityResults"
+import Axios from 'axios';
+import { Link, Route } from 'react-router-dom';
 
 export default function SearchOpportunities () {
 
+  const [opportunities, setOpportunities] = useState([]);
+  const [fetchOpportunities, setFetchOpportunities] = useState(false);
+
+  useEffect(() => {
+    async function apiCall() {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_BASE}/opportunities`
+      const response = await Axios.get(airtableURL, {
+        headers: {
+          "Authorization": `Bearer ${process.env.REACT_APP_KEY}`,
+        },
+      });
+      setOpportunities(response.data.records)
+    }
+    apiCall();
+  }, []);
+
+  console.log(opportunities)
+
   return (
-    <div style={{
-
-      position: "absolute",
-      top: "0",
-      left: "0",
-      right: "0",
-      zIndex: "3",
-      background: "gray",
-
-    }}>
+    <div>
       <form className="OpportunitiesSearchForm" style={{
 
-        margin: "25px",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        right: "0",
+        zIndex: "3",
+        background: "gray",
 
       }}>
-        <input type="text" id="opportunitySearchInput" name="opportunitySearchInput" />
+        <input type="text" id="opportunitySearchInput" name="opportunitySearchInput" style={{
+
+          marginTop: "25px",
+          marginBottom: "25px",
+
+        }} />
         <button>Submit</button>
       </form>
-      <OpportunityResults />
+      <Link to={`/tracker/opportunities/:${opportunities.id}`} key={opportunities.id} >
+        <OpportunityResults opportunities={opportunities} />
+      </Link>
     </div>
   )
 }
--- profesh/src/Components/Tracker/Search/OpportunityResults.jsx
+++ profesh/src/Components/Tracker/Search/OpportunityResults.jsx
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function OpportunityResults (props) {
+  
+  console.log(props)
+  const { opportunities } = props;
+  console.log(opportunities);
 
 
-export default class OpportunityResults extends Component {
-
-  render() {
-    return (
+  return (
       <div className="opportunitiesResults">
+      <div className="placeholder" style={{
+          
+        height: "70px",
+        
+        }}>
 
+        </div>
+        {opportunities.map((opportunity, idx) => {
+          return (
+            <div id={idx} style={{
+              
+              boxShadow: '0px 8px 10px darkgray',
+              padding: "5px",
+              margin: "10px",
+
+              display: "flex",
+              justifyContent: "flex-start",
+              flexDirection: "column",
+            }}>
+              <h1>{opportunity.fields.company_name}</h1><br />
+              <h3>{opportunity.fields.job_title}</h3>
+            </div>
+          )
+        })
+        }
       </div>
     )
-  }
 }
+
+export default OpportunityResults;
