1d7c4f00f71426bbcefe19c2a0afdc6b280d19b5
Tyler Huyser <tyler.huyser@gmail.com>
2023-01-20 23:08:55 -0500

    edited Opportunity form defaultValues, platholder, and removed label copy.

--- profesh/src/newComponents/OpportunityForm.jsx
+++ profesh/src/newComponents/OpportunityForm.jsx
@@ -33,15 +33,13 @@ export default function OpportunityForm (props) {
         
       <div className='job-form-inputs-container'>
       <div className='job-form-copy' id='job-form-title'>{formMode === "update tracked job" ? "Update Opportunity" : "Add a New Opportunity" }</div>
-        {/* <label className='job-form-input-label' id={`company-name-label`} htmlFor="companyName">Company Name:</label> */}
-            <input className="job-form-input" id={`company-name-input`} type="text" name="companyName" defaultValue={activeJob ? activeJob.fields.companyName : 'Company Name'} value={inputValues.companyName} onChange={(e) => handleInputChange(e, setInputValues)} />
+
+            <input className="job-form-input" id={`company-name-input`} type="text" name="companyName" placeholder='Company Name' value={inputValues.companyName} onChange={(e) => handleInputChange(e, setInputValues)} />
               
-        {/* <label className='job-form-input-label' id={`job-title-label`} htmlFor="jobTitle">Job Title:</label> */}
-        <input className="job-form-input" id={`job-title-input`} type="text" name="jobTitle" defaultValue={activeJob ? activeJob.fields.jobTitle : 'Job Title'} value={inputValues.jobTitle} onChange={(e) => handleInputChange(e, setInputValues)} />
+        <input className="job-form-input" id={`job-title-input`} type="text" name="jobTitle" placeholder='Job Title' value={inputValues.jobTitle} onChange={(e) => handleInputChange(e, setInputValues)} />
               
-        {/* <label className='job-form-select-label' id={`seniority-level-label`} htmlFor="seniorityLevel">Seniority Level:</label> */}
-          <select className="job-form-select" id={`seniority-level-select`} placeholder="Select option" name="seniorityLevel" defaultValue={activeJob ? activeJob.fields.seniorityLevel : 'Seniority Level'} value={inputValues.seniorityLevel} onChange={(e) => handleInputChange(e, setInputValues)} >
-            <option className='job-form-select-option' id={`seniority-level-select-option-disabled-value`} disabled value={activeJob ? activeJob.fields.seniorityLevel : 'Seniority Level'}>{activeJob ? activeJob.fields.seniorityLevel : 'Seniority Level'}</option>
+          <select className="job-form-select" id={`seniority-level-select`} name="seniorityLevel" value={inputValues.seniorityLevel} onChange={(e) => handleInputChange(e, setInputValues)} >
+          <option className='job-form-select-option' id={`seniority-level-select-option-disabled-value`} value='' disabled selected>{inputValues.seniorityLevel ? inputValues.seniorityLevel : '-- Select Seniority Level --'}</option>
             <option className='job-form-select-option' id={`seniority-level-select-option-entry-level`} value="Entry Level">Entry-Level</option>
             <option className='job-form-select-option' id={`seniority-level-select-option-associate`} value="Associate">Associate</option>
             <option className='job-form-select-option' id={`seniority-level-select-option-manager`} value="Manager">Manager</option>
@@ -50,9 +48,8 @@ export default function OpportunityForm (props) {
             <option className='job-form-select-option' id={`seniority-level-select-option-cxo`} value="CXO">CXO</option>
           </select>
 
-        {/* <label className='job-form-select-label' id={`employment-type-label`} htmlFor="employmentType">Employment Type:</label> */}
-          <select className="job-form-select" id={`employment-type-input`} name="employmentType" defaultValue={activeJob ? activeJob.fields.employmentType : "Employment Type"} value={inputValues.employmentType} onChange={(e) => handleInputChange(e, setInputValues)} >
-            <option className='job-form-select-option' id={`employment-type-select-option-disabled-value`} disabled value={activeJob ? activeJob.fields.employmentType : "Employment Type"}>{activeJob ? activeJob.fields.employmentType : "Employment Type"}</option>
+          <select className="job-form-select" id={`employment-type-input`} name="employmentType" value={inputValues.employmentType} onChange={(e) => handleInputChange(e, setInputValues)} >
+          <option className='job-form-select-option' id={`employment-type-select-option-disabled-value`} value='' disabled selected>{inputValues.employmentType ? inputValues.employmentType : '-- Select Employment Type --'}</option>
             <option className='job-form-select-option' id={`employment-type-select-option-full-time`} value="Full-Time">Full-Time</option>
             <option className='job-form-select-option' id={`employment-type-select-option-part-time`} value="Part-Time">Part-Time</option>
             <option className='job-form-select-option' id={`employment-type-select-option-contract`} value="Contract">Contract</option>
@@ -61,15 +58,12 @@ export default function OpportunityForm (props) {
             <option className='job-form-select-option' id={`employment-type-select-option-volunteer`} value="Volunteer">Volunteer</option>
           </select>
 
-        {/* <label className='job-form-input-label' id={`location-label`} htmlFor="location">Location:</label> */}
-          <input className="job-form-input" id={`location-input`} type="text" name="location" defaultValue={activeJob ? activeJob.fields.location  : 'Location'} value={inputValues.location} onChange={(e) => handleInputChange(e, setInputValues)} />
+          <input className="job-form-input" id={`location-input`} type="text" name="location" placeholder='Location' value={inputValues.location} onChange={(e) => handleInputChange(e, setInputValues)} />
           
-        {/* <label className='job-form-input-label' id={`job-description-label`} htmlFor="jobDescription">Job Description:</label> */}
-          <input className="job-form-input" id={`job-description-input`} type="text" name="jobDescription" defaultValue={activeJob ? activeJob.fields.jobDescription : "Job Description"} value={inputValues.jobDescription} onChange={(e) => handleInputChange(e, setInputValues)} />
+          <input className="job-form-input" id={`job-description-input`} type="text" name="jobDescription" placeholder='Job Description' value={inputValues.jobDescription} onChange={(e) => handleInputChange(e, setInputValues)} />
           
-        {/* <label className='job-form-select-label' htmlFor="opportunityStatus">Opportunity Status</label> */}
-          <select className="job-form-select" name="opportunityStatus" id={`job-status-input`} defaultValue={activeJob ? activeJob.fields.opportunityStatus : "Opportunity Status"} value={inputValues.opportunityStatus} onChange={(e) => handleInputChange(e, setInputValues)}>
-            <option className='job-form-select-option' id={`job-status-disabled-value`}disabled value={activeJob ? activeJob.fields.opportunityStatus : "Opportunity Status"}> {activeJob ? activeJob.fields.opportunityStatus : "Opportunity Status"} </option>
+          <select className="job-form-select" name="opportunityStatus" id={`job-status-input`} value={inputValues.opportunityStatus} onChange={(e) => handleInputChange(e, setInputValues)} required>
+          <option className='job-form-select-option' id={`job-status-disabled-value`} value="" disabled selected>{inputValues.opportunityStatus ? inputValues.opportunityStatus : '-- Select Opportunity Status --'}</option>
             <option className='job-form-select-option' id={`job-status-applied`}value="Applied">Applied</option>
             <option className='job-form-select-option' id={`job-status-phone-screen`}value="Phone Screening">Phone Screening</option>
             <option className='job-form-select-option' id={`job-status-phone-screen-pending-descision`}value="Phone Screening, Pending Decision">Phone Screening, Pending Decision</option>
@@ -80,9 +74,8 @@ export default function OpportunityForm (props) {
             <option className='job-form-select-option' id={`job-status-negotiation`} value="Negotiation">Negotiation</option>
           </select>
               
-        {/* <label className='job-form-select-label' htmlFor="actionItems">Action Item(s):</label> */}
-          <select className="job-form-select" id={`action-items-input`} type="text" name="actionItems" defaultValue={activeJob ? activeJob.fields.actionItems : 'Action Items'} value={inputValues.actionItems} onChange={(e) => handleInputChange(e, setInputValues)} >
-            <option className='job-form-select-option' id={`action-items-disabled-value`} disabled value={activeJob ? activeJob.fields.actionItems : 'Action Items'}> Select An Option </option>
+          <select className="job-form-select" id={`action-items-input`} type="text" name="actionItems" value={inputValues.actionItems} onChange={(e) => handleInputChange(e, setInputValues)} >
+            <option className='job-form-select-option' id={`action-items-disabled-value`} value='' disabled selected>{inputValues.actionItems ? inputValues.actionItems : '-- Select Action Item --'}</option>
             <option className='job-form-select-option' id={`action-items-prepare-resume`} value="Prepare Resume">Prepare Resume</option>
             <option className='job-form-select-option' id={`action-items-write-cover-letter`} value="Write Cover Letter">Write Cover Letter</option>
             <option className='job-form-select-option' id={`action-items-submit-application`} value="Submit Application">Submit Application</option>
@@ -92,18 +85,13 @@ export default function OpportunityForm (props) {
             <option className='job-form-select-option' id={`action-items-complete-project`} value="Complete Case Study/Project">Complete Case Study/Project</option>
           </select>
               
-        {/* <label className='job-form-input-label' id={`date-of-last-contact-label`} htmlFor="dateOfLastContact">Date of Last Contact:</label> */}
-          <input className="job-form-input" id={`date-of-last-contact-input`} type="text" name="dateOfLastContact" defaultValue={activeJob ? activeJob.fields.dateOfLastContact : 'Date of Last Contact'} value={inputValues.dateOfLastContact} onChange={(e) => handleInputChange(e, setInputValues)} />
+          <input className="job-form-input" id={`date-of-last-contact-input`} type="text" name="dateOfLastContact" placeholder='Date of Last Contact' value={inputValues.dateOfLastContact} onChange={(e) => handleInputChange(e, setInputValues)} />
           
-        {/* <label className='job-form-input-label' id={`contact-name-label`} htmlFor="contactName">Contact Name:</label> */}
-          <input className="job-form-input" id={`contact-name-input`} type="text" name="contactName" defaultValue={activeJob ? activeJob.fields.contactName : 'Contact Name'} value={inputValues.contactName} onChange={(e) => handleInputChange(e, setInputValues)} />
+          <input className="job-form-input" id={`contact-name-input`} type="text" name="contactName" placeholder='Contact Name' value={inputValues.contactName} onChange={(e) => handleInputChange(e, setInputValues)} />
               
-        {/* <label className='job-form-input-label' id={`contact-email-label`} htmlFor="contactEmail">Contact E-Mail:</label> */}
-          <input className="job-form-input" id={`contact-email-input`} type="text" name="contactEmail" defaultValue={activeJob ? activeJob.fields.contactEmail : "Contact Email"} value={inputValues.contactEmail} onChange={(e) => handleInputChange(e, setInputValues)} />
+          <input className="job-form-input" id={`contact-email-input`} type="text" name="contactEmail" placeholder='Contact Email' value={inputValues.contactEmail} onChange={(e) => handleInputChange(e, setInputValues)} />
               
-        {/* <label className='job-form-input-label' id={`contact-phone-number-label`} htmlFor="contactPhoneNumber">Contact Phone Number:</label> */}
-          <input className="job-form-input" id={`contact-phone-number-input`} type="text" name="contactPhoneNumber" defaultValue={activeJob ? activeJob.fields.contactPhoneNumber : "Contact Email"} value={inputValues.contactPhoneNumber} onChange={(e) => handleInputChange(e, setInputValues)} />
-        
+          <input className="job-form-input" id={`contact-phone-number-input`} type="text" name="contactPhoneNumber" placeholder='Contact Phone Number'  value={inputValues.contactPhoneNumber} onChange={(e) => handleInputChange(e, setInputValues)} />
         
         <div className="job-form-buttons-container">
           <button className="job-form-button" id="job-form-submit-button" type="submit" onClick={(e) => validateForm(e, activeJob.ID, inputValues, fetchOpportunities, setFetchOpportunities, visibility, setVisibility)} >
