69d27a70cbaf958c68e4d5875c2d2ece9e8e2c7e
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-18 14:38:12 -0400

    fixed tracked job cards expanded container contents -

--- profesh/src/components/ExpandedJobCard.jsx
+++ profesh/src/components/ExpandedJobCard.jsx
@@ -7,25 +7,54 @@ export default function ExpandedJobCard(job, setJobs, location, expanded, visibi
   
   if (expanded.includes(job.id) && location === "/tracker") {
     return (
-      <div className="expanded-container">
 
-        <p className="job-description"> {job.fields.jobDescription}</p>
-  
-          <label className="expanded-label" id="date-of-last-contact-label" forhtml="dateOfLastContact">Date of Last Contact:</label>
-          <p className="expanded-copy" id="date-of-last-contact" name="dateOflastContact">{job.fields.dateOfLastContact}</p>
-          <label className="expanded-label" id="contact-name-label" forhtml="contactName">Contact Name:</label>
-          <p className="expanded-copy" id="contact-name" name="contactName">{job.fields.contactName}</p>
-          <label className="expanded-label" id="contact-phone-number-label" forhtml="contactPhoneNumber">Contact Phone Number:</label>
-          <p className="expanded-copy" id="contact-phone-number" name="contactPhoneNumber">{job.fields.contactPhoneNumber}</p>
-          <label className="expanded-label" id="contact-email-label" forhtml="contactEmail">Contact Email:</label>
-          <p className="expanded-copy" id="contact-email" name="contactEmail">{job.fields.contactEmail}</p>
+      <>
+        <div className="expanded-container">
+
+          <p className="job-description"> {job.fields.jobDescription}</p>
     
-          <div className="expanded-job-card-buttons-container">
-            <button className="expanded-job-card-button" id="update-tracked-job-button" onMouseDown={() => handleOpenForm("update tracked job", setFormMode, visibility, setVisibility, job, setActiveJob)}> Edit </button>
-            <button className="expanded-job-card-button" id="delete-tracked-job-button" onClick={() => deleteJob(job.id, setJobs)} >Delete</button>
+            <label className="expanded-label" id="date-of-last-contact-label" forhtml="dateOfLastContact">Date of Last Contact:</label>
+            <p className="expanded-copy" id="date-of-last-contact" name="dateOflastContact">{job.fields.dateOfLastContact}</p>
+            <label className="expanded-label" id="contact-name-label" forhtml="contactName">Contact Name:</label>
+            <p className="expanded-copy" id="contact-name" name="contactName">{job.fields.contactName}</p>
+            <label className="expanded-label" id="contact-phone-number-label" forhtml="contactPhoneNumber">Contact Phone Number:</label>
+            <p className="expanded-copy" id="contact-phone-number" name="contactPhoneNumber">{job.fields.contactPhoneNumber}</p>
+            <label className="expanded-label" id="contact-email-label" forhtml="contactEmail">Contact Email:</label>
+            <p className="expanded-copy" id="contact-email" name="contactEmail">{job.fields.contactEmail}</p>
+      
+            <div className="expanded-job-card-buttons-container">
+              <button className="expanded-job-card-button" id="update-tracked-job-button" onMouseDown={() => handleOpenForm("update tracked job", setFormMode, visibility, setVisibility, job, setActiveJob)}> Edit </button>
+              <button className="expanded-job-card-button" id="delete-tracked-job-button" onClick={() => deleteJob(job.id, setJobs)} >Delete</button>
+            </div>
+          
+        </div>
+
+        <div className="expanded-container-desktop">
+
+          <div className="expanded-job-description-container">
+            <p className="expanded-description-title">JOB DESCRIPTION:</p>
+            <p className="job-description-desktop"> {job.fields.jobDescription}</p>
           </div>
-        
-      </div>
+
+          <div className="additional-job-details-expanded-container">
+            <label className="expanded-label" id="date-of-last-contact-label" forhtml="dateOfLastContact">Date of Last Contact:</label>
+            <p className="expanded-copy" id="date-of-last-contact" name="dateOflastContact">{job.fields.dateOfLastContact}</p>
+            <label className="expanded-label" id="contact-name-label" forhtml="contactName">Contact Name:</label>
+            <p className="expanded-copy" id="contact-name" name="contactName">{job.fields.contactName}</p>
+            <label className="expanded-label" id="contact-phone-number-label" forhtml="contactPhoneNumber">Contact Phone Number:</label>
+            <p className="expanded-copy" id="contact-phone-number" name="contactPhoneNumber">{job.fields.contactPhoneNumber}</p>
+            <label className="expanded-label" id="contact-email-label" forhtml="contactEmail">Contact Email:</label>
+            <p className="expanded-copy" id="contact-email" name="contactEmail">{job.fields.contactEmail}</p>
+
+            <div className="expanded-job-card-buttons-container">
+              <button className="expanded-job-card-button" id="update-tracked-job-button" onMouseDown={() => handleOpenForm("update tracked job", setFormMode, visibility, setVisibility, job, setActiveJob)}> Edit </button>
+              <button className="expanded-job-card-button" id="delete-tracked-job-button" onClick={() => deleteJob(job.id, setJobs)} >Delete</button>
+            </div>
+            
+          </div>
+          
+        </div>
+      </>
     )
   } else if (expanded.includes(job.id) && location === "/jobs") {
     return (
