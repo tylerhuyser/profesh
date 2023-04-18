7246882899799114cdbb694abc9138b5fd2018f0
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-18 17:48:43 -0400

    resolved final row issue with cards.

--- profesh/src/functions/toggleExpand.js
+++ profesh/src/functions/toggleExpand.js
@@ -1,6 +1,21 @@
 export default function toggleExpand(id, expanded, setExpanded) {
 
   let jobCard = document.getElementById(`${id}`)
+  let newJobCard = document.getElementById(`${id}`)
+  let oldJobCard = document.getElementById(`${expanded[0]}`)
+  // if (!expanded.includes(id)) {
+  //   jobCard.classList.add('expanded')
+  //   setExpanded(prevExpand => {
+  //     return [...prevExpand, id]
+  //   });
+  // };
+  // if (expanded.includes(id)) {
+  //   jobCard.classList.remove('expanded')
+  //   setExpanded(prevExpand => {
+  //     console.log(prevExpand);
+  //     return (prevExpand.filter(e => e !== id))
+  //   })
+  // };
 
   if (!expanded.includes(id)) {
     jobCard.classList.add('expanded')
