a733681ad5a7f2f2a618fe28304e0a557e03fd3a
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-19 11:27:07 -0400

    added min and max hieght to jobcard in desktop.

--- profesh/src/components/ExpandedJobCard.jsx
+++ profesh/src/components/ExpandedJobCard.jsx
@@ -37,14 +37,21 @@ export default function ExpandedJobCard(job, setJobs, location, expanded, visibi
           </div>
 
           <div className="additional-job-details-expanded-container">
-            <label className="expanded-label" id="date-of-last-contact-label" forhtml="dateOfLastContact">Date of Last Contact:</label>
-            <p className="expanded-copy" id="date-of-last-contact" name="dateOflastContact">{job.fields.dateOfLastContact}</p>
-            <label className="expanded-label" id="contact-name-label" forhtml="contactName">Contact Name:</label>
-            <p className="expanded-copy" id="contact-name" name="contactName">{job.fields.contactName}</p>
-            <label className="expanded-label" id="contact-phone-number-label" forhtml="contactPhoneNumber">Contact Phone Number:</label>
-            <p className="expanded-copy" id="contact-phone-number" name="contactPhoneNumber">{job.fields.contactPhoneNumber}</p>
-            <label className="expanded-label" id="contact-email-label" forhtml="contactEmail">Contact Email:</label>
-            <p className="expanded-copy" id="contact-email" name="contactEmail">{job.fields.contactEmail}</p>
+
+              <label className="expanded-label" id="date-of-last-contact-label" forhtml="dateOfLastContact">Date of Last Contact:</label>
+              <p className="expanded-copy" id="date-of-last-contact" name="dateOflastContact">{job.fields.dateOfLastContact}</p>
+
+              <label className="expanded-label" id="contact-name-label" forhtml="contactName">Contact Name:</label>
+              <p className="expanded-copy" id="contact-name" name="contactName">{job.fields.contactName}</p>
+
+
+              <label className="expanded-label" id="contact-phone-number-label" forhtml="contactPhoneNumber">Contact Phone Number:</label>
+              <p className="expanded-copy" id="contact-phone-number" name="contactPhoneNumber">{job.fields.contactPhoneNumber}</p>
+
+
+              <label className="expanded-label" id="contact-email-label" forhtml="contactEmail">Contact Email:</label>
+              <p className="expanded-copy" id="contact-email" name="contactEmail">{job.fields.contactEmail}</p>
+
 
             <div className="expanded-job-card-buttons-container">
               <button className="expanded-job-card-button" id="update-tracked-job-button" onMouseDown={() => handleOpenForm("update tracked job", setFormMode, visibility, setVisibility, job, setActiveJob)}> Edit </button>
