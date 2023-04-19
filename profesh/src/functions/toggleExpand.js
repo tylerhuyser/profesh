9338ee2226216c9d558957276391d7cb0ee6ac06
Tyler Huyser <tyler.huyser@gmail.com>
2023-04-18 20:56:36 -0400

    revised toggleExpand function for new layout.

--- profesh/src/functions/toggleExpand.js
+++ profesh/src/functions/toggleExpand.js
@@ -1,8 +1,9 @@
 export default function toggleExpand(id, expanded, setExpanded) {
 
-  let jobCard = document.getElementById(`${id}`)
-  let newJobCard = document.getElementById(`${id}`)
-  let oldJobCard = document.getElementById(`${expanded[0]}`)
+  // OLD TOGGLE EXPAND METHOD
+
+  // let jobCard = document.getElementById(`${id}`)
+
   // if (!expanded.includes(id)) {
   //   jobCard.classList.add('expanded')
   //   setExpanded(prevExpand => {
@@ -17,17 +18,23 @@ export default function toggleExpand(id, expanded, setExpanded) {
   //   })
   // };
 
+  // NEW TOGGLE EXPAND METHOD
+
+  let newJobCard = document.getElementById(`${id}`)
+  console.log(expanded[0])
+  let oldJobCard = document.getElementById(`${expanded[0]}`)
+
+  if (!expanded.includes(id) && oldJobCard) {
+    oldJobCard.classList.remove('expanded')
+    newJobCard.classList.add('expanded')
+    setExpanded([id])
+  } 
   if (!expanded.includes(id)) {
-    jobCard.classList.add('expanded')
-    setExpanded(prevExpand => {
-      return [...prevExpand, id]
-    });
-  };
+    newJobCard.classList.add('expanded')
+    setExpanded([id])
+  } 
   if (expanded.includes(id)) {
-    jobCard.classList.remove('expanded')
-    setExpanded(prevExpand => {
-      console.log(prevExpand);
-      return (prevExpand.filter(e => e !== id))
-    })
-  };
+    oldJobCard.classList.remove('expanded')
+    setExpanded([])
+  }
 }
